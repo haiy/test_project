f33b489b8256f742e24f33359d3832bca8dd0383
haiy <haiy@users.noreply.github.com>
2016-07-28 17:17:41 +0800

    Create HelloWorldTest.java

---
+++ spark_test/src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+
+import org.junit.Test;
+
+/**
+ * Created by dz-h on 16-7-28.
+ */
+public class HelloWorldTest {
+    @Test
+    public void HelloWorldTest(){
+        System.out.println("HelloWorld");
+    }
+}
